8c9b45d7fe841936ecdf105a90f94924762dde82
Giovanni Shibaki <giovannishibaki@gmail.com>
2020-10-04 17:59:51 -0300

    File Manager
    Open file for read and write and some more operations

---
+++ Util/fileManager.h
@@ -0,0 +1,23 @@
+#ifndef FILEMANAGER_H
+	#define FILEMANAGER_H
+
+	#ifdef __linux__
+	    #define DIRECTORY_NAME "./Data/"
+	#endif // _WIN32
+
+	#ifdef _WIN32
+	    #define DIRECTORY_NAME ".\\Data\\"
+	#endif
+
+	#define FILE_EXTENSION ".txt"
+	#define boolean int
+	#define READLINE_BUFFER 16
+
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include <string.h>
+
+	boolean openReadFile(char *fileName);
+	boolean openWriteFile(char *fileName);
+	char *readline(FILE *stream);
+#endif
