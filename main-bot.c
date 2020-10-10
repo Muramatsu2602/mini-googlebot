36456dd753fe31944e4ad1cf07cbc8aaa94be5be
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-10-10 14:19:38 -0300

    filemanager functions in main

--- main-bot.c
+++ main-bot.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #include "Util/list.h"
-#include "Util/fileManager.h"
+// #include "Util/fileManager.h"
 
 /*
     1. Inserir um site: a inserção deve ser ordenada pela chave primária, isto é, o código do
@@ -14,12 +14,38 @@
     5. Sair: finalizar o programa.
 */
 
+boolean openReadFile(char *fileName, FILE *fp, char *mode)
+{
+    fp = fopen(fileName, mode);
+    if (fp != NULL)
+    {
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+// You can read file lines using this function
+char *readline(FILE *stream)
+{
+    char *string = NULL;
+    int pont = 0;
+    do
+    {
+        if (pont % READLINE_BUFFER == 0)
+        {
+            string = (char *)realloc(string, ((pont / READLINE_BUFFER) + 1) * READLINE_BUFFER);
+        }
+        string[pont] = (char)fgetc(stream);
+    } while (string[pont++] != '\n' && !feof(stream));
+    string[pont - 1] = '\0';
+    return string;
+}
+
 int main(int argc, char const *argv[])
 {
-    LISTA * list;
-    FILE * fp;
-
-
+    LISTA *list;
+    FILE *fp;
 
     // lista_apagar(list);
     // fclose(fp);
