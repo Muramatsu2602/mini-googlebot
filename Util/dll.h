5683ebfcb0e430f8ce2453aad342e78c18694fda
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-11-28 16:19:10 -0300

    changing lista_remover to DLL

--- Util/dll.h
+++ Util/dll.h
@@ -16,6 +16,7 @@
 	#include "item.h"
 
 	typedef struct Lista LISTA;
+	typedef struct no_ NO;
 
 	LISTA* lista_criar(void);
 	boolean lista_inserir_fim(LISTA *lista, ITEM* item);
@@ -23,11 +24,13 @@
 	boolean lista_inserir_by_relevance(LISTA *lista, ITEM *item); // Inserir itens de forma ordenada através da relevância
 	void lista_apagar(LISTA **lista); 						        		
 	boolean lista_remover(LISTA *lista, int chave);
-	ITEM* lista_busca(LISTA *lista, int chave); 							
+	NO *lista_busca_ordenada(LISTA *lista, int chave);
 	void lista_busca_keyword(LISTA *lista, LISTA *key_list, char *keyword);
 	int lista_tamanho(LISTA *lista);
+	
 	boolean lista_vazia(LISTA *lista);
 	boolean lista_cheia(void);
+
 	void lista_imprimir(LISTA *lista);
 	void lista_imprimir_short(LISTA *l, int n); 
 	void lista_apagar(LISTA **lista);
