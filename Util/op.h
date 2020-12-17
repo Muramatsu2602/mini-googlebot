f1fcb0fe442ee7602fa92a46654fdcf23aaea5b3
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-12-17 10:31:16 -0300

    gitignore blocked some new files

---
+++ Util/op.h
@@ -0,0 +1,28 @@
+/*
+    11796444 - Giovanni Shibaki Camargo
+    11796451 - Pedro Kenzo Muramatsu Carmo
+*/
+
+#ifndef _OPERATION_H
+#define _OPERATION_H
+
+    #include "dll.h"
+    #include "AVL.h"
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+
+    #define BUFFER 100
+
+    FILE *openReadFile(char *fileName, FILE *fp, char *mode);
+    char *readline(FILE *stream);
+
+    boolean inserirSite(LISTA *lista, AVL *avl);
+    void removerSite(LISTA *lista, AVL *avl);
+    void inserirPalavraChave(LISTA *lista, AVL *avl);
+    void atualizarRelevancia(LISTA *lista);
+    void buscarPorKeyword(LISTA *lista, AVL *avl);
+    void sugerirSites(LISTA *lista, AVL *avl);
+
+#endif
